dfba7470b17a7de6a1e666e88549d96bd74c9a00
Denis Snitko <denissnitko@gmail.com>
2020-09-25 13:19:55 +0300

    Fix monor bugs

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,121 +1,128 @@
+document.addEventListener('DOMContentLoaded', () => {
 
-const elMinusBtn = document.querySelectorAll('.js-minus');
+  const elMinusBtn = document.querySelectorAll('.js-minus');
 
-const elPlusBtn = document.querySelectorAll('.js-plus');
+  const elPlusBtn = document.querySelectorAll('.js-plus');
 
-const subMenuTrigger = document.querySelector('.menu__item--catalog');
+  const subMenuTrigger = document.querySelector('.menu__item--catalog');
 
-const filterTrigger = document.querySelectorAll('.filter__sub-title');
-const filterSubTrigger = document.querySelector('.p-assortment__filter-title--trigger');
-const mainFilter = document.querySelector('.p-assortment__filter');
+  const filterTrigger = document.querySelectorAll('.filter__sub-title');
 
-const burgerTrigger = document.querySelector('.burger-menu__icon');
+  const filterSubTrigger = document.querySelector('.p-assortment__filter-title--trigger');
+  const mainFilter = document.querySelector('.p-assortment__filter');
 
-const regionTrigger = document.querySelector('.top-header__region span');
-const regionPopup = document.querySelector('.region-popup');
+  const burgerTrigger = document.querySelector('.burger-menu__icon');
 
-let swiper = new Swiper('.swiper-container-hero', {
-  loop: true,
+  const regionTrigger = document.querySelector('.top-header__region span');
+  const regionPopup = document.querySelector('.region-popup');
 
-  navigation: {
-    nextEl: '.swiper-hero-button-next',
-    prevEl: '.swiper-hero-button-prev'
-  },
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-});
-
-
-let itemSwiper = document.querySelectorAll('.swiper-container-wrapper');
-itemSwiper.forEach(swiper => {
-  new Swiper(swiper.children, {
-    slidesPerView: 1,
-    // spaceBetween: 60,
-
-    breakpoints: {
-      460: {
-        slidesPerView: 2,
-        spaceBetween: 50,
-
-      },
-      576: {
-        slidesPerView: 2,
-        spaceBetween: 50,
-
-      },
-      768: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-      992: {
-        slidesPerView: 5,
-        spaceBetween: 20,
-      }
-    },
+  let swiper = new Swiper('.swiper-container-hero', {
+    loop: true,
 
     navigation: {
-      nextEl: swiper.querySelector('.swiper-button-next'),
-      prevEl: swiper.querySelector('.swiper-button-prev')
-    }
+      nextEl: '.swiper-hero-button-next',
+      prevEl: '.swiper-hero-button-prev'
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+
   });
-})
 
 
-elMinusBtn.forEach(el => {
-  el.addEventListener('click', (event) => {
-    let input = event.target.nextElementSibling;
-    if (input.value == 1) {
-      input.value = 1;
-    } else {
-      input.value--;
-    }
+  let itemSwiper = document.querySelectorAll('.swiper-container-wrapper');
+  itemSwiper.forEach(swiper => {
+    new Swiper(swiper.children, {
+      slidesPerView: 1,
+      // spaceBetween: 60,
+
+      breakpoints: {
+        460: {
+          slidesPerView: 2,
+          spaceBetween: 50,
+
+        },
+        576: {
+          slidesPerView: 2,
+          spaceBetween: 50,
+
+        },
+        768: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+        992: {
+          slidesPerView: 5,
+          spaceBetween: 20,
+        }
+      },
+
+      navigation: {
+        nextEl: swiper.querySelector('.swiper-button-next'),
+        prevEl: swiper.querySelector('.swiper-button-prev')
+      }
+    });
   })
-})
 
-elPlusBtn.forEach(el => {
-  el.addEventListener('click', (event) => {
-    let input = event.target.previousElementSibling;
-    input.value++;
+
+  elMinusBtn.forEach(el => {
+    el.addEventListener('click', (event) => {
+      let input = event.target.nextElementSibling;
+      if (input.value == 1) {
+        input.value = 1;
+      } else {
+        input.value--;
+      }
+    })
   })
-})
 
-// SUB-MENU HIDE / SHOW
-
-subMenuTrigger.addEventListener('click', (event) => {
-  event.target.querySelector('.sub-menu').classList.toggle('sub-menu--show')
-})
-
-// FILTER HIDE / SHOW
-
-filterTrigger.forEach((el) => {
-  el.addEventListener('click', (event) => {
-    event.target.classList.toggle('filter__sub-title--arrow-right')
-    event.target.nextElementSibling.classList.toggle('hide-filter-list')
+  elPlusBtn.forEach(el => {
+    el.addEventListener('click', (event) => {
+      let input = event.target.previousElementSibling;
+      input.value++;
+    })
   })
-})
 
-if (!filterTrigger) {
-  filterSubTrigger.addEventListener('click', () => {
-    if (mainFilter.style.display == 'block')
-      mainFilter.setAttribute('style', 'display: none')
-    else {
-      mainFilter.setAttribute('style', 'display: block')
-    }
+  // SUB-MENU HIDE / SHOW
+
+  subMenuTrigger.addEventListener('click', (event) => {
+    event.target.querySelector('.sub-menu').classList.toggle('sub-menu--show')
   })
-}
+
+  // FILTER HIDE / SHOW
+
+  filterTrigger.forEach((el) => {
+    el.addEventListener('click', (event) => {
+      event.target.classList.toggle('filter__sub-title--arrow-right')
+      event.target.nextElementSibling.classList.toggle('hide-filter-list')
+    })
+  })
+
+  if (filterSubTrigger) {
+    filterSubTrigger.addEventListener('click', () => {
+      if (mainFilter.style.display == 'block')
+        mainFilter.setAttribute('style', 'display: none')
+      else {
+        mainFilter.setAttribute('style', 'display: block')
+      }
+    })
+    console.log('true');
+  } else {
+    console.log(1);
+  }
 
 
 
-// BURGER HIDE / SHOW
+  // BURGER HIDE / SHOW
 
-burgerTrigger.addEventListener('click', function (event) {
-  this.nextElementSibling.classList.toggle('burger-menu--show')
-})
+  burgerTrigger.addEventListener('click', function (event) {
+    this.nextElementSibling.classList.toggle('burger-menu--show')
+  })
 
-// REGION
+  // REGION
 
-regionTrigger.addEventListener('click', () => {
-  regionPopup.classList.toggle('region-popup--show')
-})
+  regionTrigger.addEventListener('click', () => {
+    regionPopup.classList.toggle('region-popup--show')
+  })
+
+});
